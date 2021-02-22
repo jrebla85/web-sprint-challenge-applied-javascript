0970c5959fe0fa031266c3b6fb2f6b068d37007c
jeremy black <jrebla85@yahoo.com>
2021-02-21 19:48:34 -0600

    worked on tsk 5 & 6

--- src/components/card.js
+++ src/components/card.js
@@ -1,4 +1,32 @@
+import axios from 'axios';
+
 const Card = (article) => {
+  const card = document.createElement('div');
+  card.classList.add('card')
+
+  const headline = document.createElement('div');
+  headline.classList.add('headline');
+  headline.textContent = article.headline;
+
+  const author = document.createElement('div');
+  author.classList.add('author');
+
+  const imgContainer = document.createElement('div');
+  imgConstiner.classList.add('img-container');
+
+  const image = document.createElement('img');
+  imgContainer.src = articles.authorPhoto;
+
+  const span = document.createElement('span');
+  span.textContent = articles.authorName;
+
+  card.appendChild(headline);
+  card.appendChild(author);
+  author.appendChild(imgContainer);
+  imgContainer.appendChild(image);
+  author.appendChild(span);
+
+  return card;
   // TASK 5
   // ---------------------
   // Implement this function, which should return the markup you see below.
@@ -20,6 +48,20 @@ const Card = (article) => {
 }
 
 const cardAppender = (selector) => {
+  axios.get('https://lambda-times-api.herokuapp.com/articles')
+  .then(({data}) => {
+    const newData = data.articles;
+    const result = Object.keys(newData).map(e => [e, newData[e]]);
+    return result;
+
+    result.forEach(e => {
+
+    })
+    const e = document.querySelector(selector);
+    const newTab = Card(result);
+    element.appendChild(newTab);
+  })
+  .catch(error => console.log('error'))
   // TASK 6
   // ---------------------
   // Implement this function that takes a css selector as its only argument.
